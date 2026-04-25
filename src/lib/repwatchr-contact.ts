47390bd56243047720ef17f4ca0eefefc9bc4b5d
Ryan Nichols <theflashflash24@gmail.com>
2026-04-24 22:24:49 -0500

    Add RepWatchr contact constants

---
+++ src/lib/repwatchr-contact.ts
@@ -0,0 +1,4 @@
+export const REPWATCHR_PHONE_DISPLAY = "(903) 961-1776";
+export const REPWATCHR_PHONE_E164 = "+19039611776";
+export const REPWATCHR_EMAIL = "Ryan@RealRyanNichols.com";
+export const REPWATCHR_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.repwatchr.com";
